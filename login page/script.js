4a5a815a81fbadda3c71bc842490b2dce77da624
mateen-dev01 <mateeneditor22@gmail.com>
2026-07-18 21:24:18 +0500

    Login Page Done

---
+++ login page/script.js
@@ -0,0 +1,51 @@
+    const form = document.getElementById("loginForm");
+    const email = document.getElementById("email");
+    const password = document.getElementById("password");
+    const emailError = document.getElementById("emailError");
+    const passwordError = document.getElementById("passwordError");
+
+
+
+    let eyeicon = document.getElementById("eyeicon");
+
+          eyeicon.onclick = function(){
+            if(password.type == "password"){
+                password.type = "text";
+                eyeicon.src = "eye-open.png";
+            }else{
+                password.type = "password";
+                eyeicon.src = "eye-close.png";
+            }
+          }
+
+    form.addEventListener("submit", function (event) {
+        event.preventDefault();
+        emailError.textContent = "";
+        passwordError.textContent = "";
+        let valid = true;
+
+        const emailPattern =
+          /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+            if(email.value.trim() === "") {
+                emailError.textContent = "Email is required";
+                valid = false;
+            }else if (!emailPattern.test(email.value.trim())) {
+                emailError.textContent = "Enter a valid email";
+                valid = false;
+            }
+
+            if(password.value.trim() === "") {
+                passwordError.textContent = "Password is required";
+                valid = false;
+
+            }else if (password.value.length < 8) {
+                passwordError.textContent = "Password must be at least 8 characters";
+                valid = false;
+            }
+
+            if (valid) {
+                alert("Login Successful!");
+                form.reset();
+            }
+    });
